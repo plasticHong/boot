25a31ff62f1cbcb3f71e167cac0b800459b70243
Plastic_hong <zakkslash18@naver.com>
2022-09-27 19:56:45 +0900

    error

--- src/main/java/com/spring/controller/MemberController.java
+++ src/main/java/com/spring/controller/MemberController.java
@@ -3,32 +3,44 @@ package com.spring.controller;
 import com.spring.domain.Member;
 import com.spring.exception.BadRequestException;
 import com.spring.service.MemberService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/member/*")
 @SessionAttributes("member")
 public class MemberController {
 
+    Logger log = LoggerFactory.getLogger(this.getClass());
     @Autowired
     MemberService service;
 
     @GetMapping("login")
-    public String login() {
+    public String login(HttpServletRequest request) {
+        log.info("method : " + request.getMethod() + ", url : " + request.getRequestURL());
 
         return "/member/login";
     }
 
     @PostMapping("login")
-    public String login(Member member, Model model) {
+    public String login(Member member, Model model,HttpServletRequest request) {
+        log.info("method : " + request.getMethod() + ", url : " + request.getRequestURL());
+
         Member findMember = service.getMember(member);
         if (findMember != null
                 && findMember.getPassword().equals(member.getPassword())) {
@@ -51,40 +63,39 @@ public class MemberController {
     }
 
     @GetMapping("join")
-    public String joinForm() {
-
+    public String joinForm(HttpServletRequest request) {
+        log.info("method : " + request.getMethod() + ", url : " + request.getRequestURL());
         return "/member/join";
+
     }
 
     @PostMapping("join")
     @ResponseBody
-    public Object doJoin(Member member, Model model) {
-
-        System.out.println(member.toString());
-
+    public Object doJoin(Member member, Model model, HttpServletRequest request) {
+        log.info("method : " + request.getMethod() + ", url : " + request.getRequestURL());
         try {
 
             Member joinMember = service.memberJoin(member);
 
-        }catch (BadRequestException e){
-            System.out.println(e.getMessage());
-
-            return new ResponseEntity<Member>(member,HttpStatus.BAD_REQUEST);
-
+        } catch (BadRequestException e) {
+            String msg = "이미 존재하는 회원입니다.";
+            return new ResponseEntity<>(msg, HttpStatus.BAD_REQUEST);
         }
 
-        return  new ResponseEntity<Member>(member,HttpStatus.OK);
+        HashMap<String, String > map = new HashMap<>();
+        map.put("member", "member");
+        return new ResponseEntity<>(member,HttpStatus.OK);
 
     }
 
     @GetMapping("profile")
-    public String  profile() {
+    public String profile() {
 
         return "/member/profile";
     }
 
     @PostMapping("update")
-    public String  update(Member member) {
+    public String update(Member member) {
         service.updateMember(member);
 
         return "/member/profile";
--- src/main/java/com/spring/exception/BadRequestException.java
+++ src/main/java/com/spring/exception/BadRequestException.java
@@ -6,9 +6,13 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Already exists")
 public class BadRequestException extends RuntimeException{
-
+    String message = "이미 존재하는 회원입니다";
     public BadRequestException(String message) {
         super(message);
     }
 
+    @Override
+    public String getMessage() {
+        return this.message;
+    }
 }
